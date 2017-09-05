08c935d73b6f48e43bf361bd19d104cbd9715bbb
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-05 16:56:51 +0300

    enable google oath api and secure its keys

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
 passport.use(new GoogleStrategy());
