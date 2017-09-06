75adb834b0e4392dd32d0c9117040dc1ea8418c7
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-06 16:58:51 +0300

    update passport with extend user query

--- services/passport.js
+++ services/passport.js
@@ -11,7 +11,17 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            done(null, existingUser);
+          } else {
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user))
+          }
+        });
     })
 );
