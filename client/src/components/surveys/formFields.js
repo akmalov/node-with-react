c6d30e6d17d6ccbd2445d407cd7c5efa75083207
Sergey-Akmalov <akmolgt@gmail.com>
2017-09-16 22:10:33 +0300

    config submitsurvey action

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -2,5 +2,5 @@ export default [
   {label: 'Survey Title', name: 'title', noValueError: 'Enter a Survey Title'},
   {label: 'Subject Line', name: 'subject', noValueError: 'Enter a Survey Line'},
   {label: 'Email Body', name: 'body', noValueError: 'Enter a Email Body'},
-  {label: 'Recipient List', name: 'emails', noValueError: 'Enter a Recipient List'}
+  {label: 'Recipient List', name: 'recipients', noValueError: 'Enter a Recipient List'}
 ];
